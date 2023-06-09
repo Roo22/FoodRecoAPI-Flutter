0eb39c847d6689ddcbce9c139160f1297c7d3bec
Roo22 <rawan.mustafa75@gmail.com>
2023-06-09 21:41:08 +0300

    init

---
+++ lib/Componant/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  // Constant Color
+  static const Color kDarkFuchsiaColor = Color(0xFF7D0759);
+  static const Color kCyanColor = Color(0xFF08F7FE);
+  static const Color kLightFuchsiaColor = Color(0xFFFCC7EC);
+  static const Color kBlackColor = Color(0xFF19191B);
+  static const Color kYellowColor = Color(0xFFF2A33A);
+  static const Color kWhiteColor = Color(0xFFFFFFFF);
+  static const Color kGreyColor = Color(0xFF767680);
+}
